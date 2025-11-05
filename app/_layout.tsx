ecd6b280430ed9b3618a04fff79d60652cf1f891
isadadi <idadi70@gmail.com>
2025-11-05 11:45:08 +0700

    temp

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,32 +1,45 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
+// app/_layout.tsx
+import "@/global.css";
+import { SessionProvider, useSession } from "@/stores/auth-store";
+import { SplashScreenController } from "@/utils/splash";
+import * as eva from "@eva-design/eva";
+import { ApplicationProvider, IconRegistry } from "@ui-kitten/components";
+import { EvaIconsPack } from "@ui-kitten/eva-icons";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import "react-native-reanimated";
-import "@/global.css";
+import React from "react";
+// export const unstable_settings = {
+//   initialRouteName: "(auth)",
+// };
 
-import { useColorScheme } from "@/hooks/use-color-scheme";
+export default function Layout() {
+  return (
+    <>
+      <IconRegistry icons={EvaIconsPack} />
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <SessionProvider>
+          <SplashScreenController />
+          <StatusBar style="auto" />
+          <RootNavigator />
+        </SessionProvider>
+      </ApplicationProvider>
+    </>
+  );
+}
 
-export const unstable_settings = {
-  anchor: "(tabs)",
-};
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
+// Create a new component that can access the SessionProvider context later.
+function RootNavigator() {
+  const { session } = useSession();
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="modal"
-          options={{ presentation: "modal", title: "Modal" }}
-        />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack>
+      <Stack.Protected guard={!!session}>
+        <Stack.Screen name="(app)" />
+      </Stack.Protected>
+
+      <Stack.Protected guard={!session}>
+        <Stack.Screen name="login" />
+      </Stack.Protected>
+    </Stack>
   );
 }
